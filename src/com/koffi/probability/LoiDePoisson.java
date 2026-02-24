944c65499816b1f96bbd709b68c9a4943767e367
koffi2317 <koffiakakpo23@gmail.com>
2026-02-24 16:02:23 -0500

    ajout des fonctions necessaires pour calculer comme il faut la loi de poisson avec son esperance et variance

--- src/com/koffi/probability/LoiDePoisson.java
+++ src/com/koffi/probability/LoiDePoisson.java
@@ -1,4 +1,45 @@
 package com.koffi.probability;
 
 public class LoiDePoisson {
+
+    double esperance = 0;
+    double variance = 0;
+    double probabiblite = 0;
+    int nbOccurence = 0;
+    double lambda = 0;
+    int nboccutempo = 0;
+
+
+    public LoiDePoisson(double lambda, int occurence) {
+        this.lambda = lambda;
+        this.nbOccurence = occurence;
+        nboccutempo = nbOccurence;
+    }
+
+    public void occurenceFactorielle() {
+
+        for (int i = (nbOccurence - 1); i >= 1; i--) {
+
+            nbOccurence *= i;
+        }
+
+    }
+
+    public double probdeEvent() {
+
+        probabiblite = ((Math.exp(-1 * lambda)) * (Math.pow(lambda, nboccutempo))) / (nbOccurence);
+
+        return probabiblite;
+    }
+
+    public double esperance() {
+
+        return esperance = lambda;
+    }
+
+    public double getVariance() {
+
+        return variance = lambda;
+    }
+
 }
