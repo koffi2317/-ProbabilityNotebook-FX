cb57ace17a8af51928f414f80b9a8117bf661d14
koffi2317 <koffiakakpo23@gmail.com>
2026-02-24 16:45:56 -0500

    ajout des parametres necessaire pour loi normale

--- src/com/koffi/probability/LoiNormale.java
+++ src/com/koffi/probability/LoiNormale.java
@@ -1,4 +1,29 @@
 package com.koffi.probability;
 
 public class LoiNormale {
+
+    double moyenne = 0;
+    double ecartType = 0;
+    double fctloiNormale = 0;
+    double valeurx = 0;
+
+    public LoiNormale(double moyenne, double ecartTy, double valeur) {
+
+        this.moyenne = moyenne;
+        this.ecartType = ecartTy;
+        this.valeurx = valeur;
+    }
+
+    public double fonctionLoiNormale() {
+
+        double part1 = 0;
+        part1 = ((1) / (ecartType * Math.sqrt(2 * Math.PI)));
+
+        fctloiNormale =
+                part1 * Math.exp((-1) * (Math.pow((valeurx - moyenne), 2)) / (2 * Math.pow(ecartType, 2)));
+
+        return fctloiNormale;
+    }
+
+
 }
