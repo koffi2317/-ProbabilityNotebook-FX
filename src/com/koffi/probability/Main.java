e972acf070201379086dfefb1890820315cb71f7
koffi2317 <koffiakakpo23@gmail.com>
2026-03-02 08:19:01 -0500

    update + test unitaire

--- src/com/koffi/probability/Main.java
+++ src/com/koffi/probability/Main.java
@@ -1,11 +1,18 @@
 import com.koffi.probability.LoiGeometrique;
+import com.koffi.probability.LoiNormale;
 
 public static void main(String[] args) {
 
 
     LoiGeometrique loi= new LoiGeometrique(0.34,9);
+    LoiNormale  loi2= new LoiNormale(23,6,6);
+
+
 
     System.out.println(loi.event(8));
+    System.out.println(loi2.fonctionLoiNormale());
+
+
 
 
 
