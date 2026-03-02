6ff2e48a8855ecf3a0d47873dbf2b37cdff37adb
koffi2317 <koffiakakpo23@gmail.com>
2026-03-02 09:20:45 -0500

    interaction utilisateur pour savoir quelle loi prendre

--- src/com/koffi/probability/Main.java
+++ src/com/koffi/probability/Main.java
@@ -3,8 +3,16 @@ import com.koffi.probability.LoiDePoisson;
 import com.koffi.probability.LoiGeometrique;
 import com.koffi.probability.LoiNormale;
 
+import java.util.Scanner;
+
+
 public static void main(String[] args) {
 
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Bonjour veuiller choisir une valeur entre 1 a 4. \n 1-Loi " +
+            "Binominale" + "\n 2-Loi de Poisson " + "\n 3-Loi Geometrique" +
+            "\n 4-Loi Normale");
 
     LoiGeometrique loi = new LoiGeometrique(0.34, 9);
     LoiNormale loi2 = new LoiNormale(23, 6, 6);
