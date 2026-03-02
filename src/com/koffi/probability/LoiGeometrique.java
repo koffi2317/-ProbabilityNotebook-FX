afb2eab91b4f744b55d6431c3b5cde9db364a62e
koffi2317 <koffiakakpo23@gmail.com>
2026-03-01 22:44:00 -0500

    modification du main et test unitaire pour loi geometrique

--- src/com/koffi/probability/LoiGeometrique.java
+++ src/com/koffi/probability/LoiGeometrique.java
@@ -17,6 +17,7 @@ public class LoiGeometrique {
         this.probabiblieSucces = probreu;
         this.nbessaie = nbessaie;
         probEchec = 1 - probabiblieSucces;
+        nbechec=nbessaie-1;
     }
 
     public double event() {
--- src/com/koffi/probability/Main.java
+++ src/com/koffi/probability/Main.java
@@ -1,10 +1,11 @@
-
-
+import com.koffi.probability.LoiGeometrique;
 
 public static void main(String[] args) {
 
 
+    LoiGeometrique loi= new LoiGeometrique(0.34,9);
 
+    System.out.println(loi.event());
 
 
 
