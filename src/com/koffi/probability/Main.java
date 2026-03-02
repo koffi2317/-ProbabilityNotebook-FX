1fc13a0c9eefa2858af97758e34e8170b6053be6
koffi2317 <koffiakakpo23@gmail.com>
2026-03-02 11:59:37 -0500

    modification du menu

--- src/com/koffi/probability/Main.java
+++ src/com/koffi/probability/Main.java
@@ -9,12 +9,45 @@ import java.util.Scanner;
 public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
-
+    int choixUtilisateur;
     System.out.println("Bonjour veuiller choisir une valeur entre 1 a 4. \n 1-Loi " +
             "Binominale" + "\n 2-Loi de Poisson " + "\n 3-Loi Geometrique" +
             "\n 4-Loi Normale");
 
-    LoiGeometrique loi = new LoiGeometrique(0.34, 9);
+    choixUtilisateur = input.nextInt();
+
+
+    do {
+
+        System.out.println("Veuiller entrer un choix valide svp");
+        choixUtilisateur = input.nextInt();
+
+    } while (choixUtilisateur < 1 || choixUtilisateur > 4);
+
+
+    if (choixUtilisateur == 1) {
+        LoiBinomiale binomiale = new LoiBinomiale(5, 2, 0.67);
+        binomiale.valeurCoefficientBinomial();
+        binomiale.valeurPobetEchec();
+        System.out.println(binomiale.valeurdeprobBinomiale());
+    } else if (choixUtilisateur == 2) {
+
+        LoiDePoisson poisson = new LoiDePoisson(9.54, 23);
+        poisson.occurenceFactorielle();
+        System.out.println(poisson.probdeEvent());
+    } else if (choixUtilisateur == 3) {
+
+        LoiGeometrique loi = new LoiGeometrique(0.34, 9);
+        System.out.println(loi.event(8));
+    } else if (choixUtilisateur == 4) {
+
+        LoiNormale loi2 = new LoiNormale(23, 6, 6);
+        System.out.println(loi2.fonctionLoiNormale());
+
+    }
+
+
+   /* LoiGeometrique loi = new LoiGeometrique(0.34, 9);
     LoiNormale loi2 = new LoiNormale(23, 6, 6);
     LoiDePoisson poisson = new LoiDePoisson(9.54, 23);
     LoiBinomiale binomiale = new LoiBinomiale(5, 2, 0.67);
@@ -25,8 +58,7 @@ public static void main(String[] args) {
 
     System.out.println(loi.event(8));
     System.out.println(loi2.fonctionLoiNormale());
-    System.out.println(poisson.probdeEvent());
-    System.out.println(binomiale.valeurdeprobBinomiale());
+    System.out.println(poisson.probdeEvent());*/
 
 
 }
