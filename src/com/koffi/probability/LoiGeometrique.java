ba2da1ff15f64c43f382e68411f0e662d45a81d8
koffi2317 <koffiakakpo23@gmail.com>
2026-03-01 22:07:58 -0500

    ajout de fonction de base pour la LoiGeometrique.java

--- src/com/koffi/probability/LoiGeometrique.java
+++ src/com/koffi/probability/LoiGeometrique.java
@@ -1,4 +1,43 @@
 package com.koffi.probability;
 
 public class LoiGeometrique {
+
+    double probabiblieSucces;
+    int nbessaie;
+    int nbechec;
+    double probEchec;
+    double esperance;
+    double variance;
+    double eventprob;
+
+    public LoiGeometrique(double probreu, int nbessaie) {
+
+        this.probabiblieSucces = probreu;
+        this.nbessaie = nbessaie;
+        probEchec = 1 - probabiblieSucces;
+    }
+
+    public double event() {
+
+        eventprob = (Math.pow(probEchec, nbechec) * probabiblieSucces);
+
+        return eventprob;
+    }
+
+
+    public double esperanceGeometrique() {
+
+        esperance = (1 / probabiblieSucces);
+
+        return esperance;
+    }
+
+    public double varianceGeometrique() {
+
+        variance = ((probEchec) / Math.pow(probabiblieSucces, 2));
+
+        return variance;
+    }
+
+
 }
