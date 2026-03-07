93399cbbda7ec505c3454adc3405225703947fba
koffi2317 <koffiakakpo23@gmail.com>
2026-03-07 09:37:06 -0500

    suppression de lignes de codes non necessaire

--- src/com/koffi/probability/Main.java
+++ src/com/koffi/probability/Main.java
@@ -70,20 +70,7 @@ public static void main(String[] args) {
         System.out.println(loi2.fonctionLoiNormale());
 
     }
-
-
-   /* LoiGeometrique loi = new LoiGeometrique(0.34, 9);
-    LoiNormale loi2 = new LoiNormale(23, 6, 6);
-    LoiDePoisson poisson = new LoiDePoisson(9.54, 23);
-    LoiBinomiale binomiale = new LoiBinomiale(5, 2, 0.67);
-
-    poisson.occurenceFactorielle();
-    binomiale.valeurCoefficientBinomial();
-    binomiale.valeurPobetEchec();
-
-    System.out.println(loi.event(8));
-    System.out.println(loi2.fonctionLoiNormale());
-    System.out.println(poisson.probdeEvent());*/
+    
 
 
 }
